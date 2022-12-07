217cb94d8fd2c81418dfa3ff0a89f931a016e4e1
Ameen.Maria.S@gmail.com <redmomo@Marias-MacBook-Air.local>
2022-12-07 15:04:40 -0500

    rendered cart

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,17 +1,26 @@
 import React from 'react';
+import CartContainer from './CartContainer';
+function Cart({current_cart}) {
 
-function Cart() {
 
+    if (current_cart.products.first.exist) {
+    // {if cart.last.products.first.exist ?
+
+    return (
+            <div className="about-us">
+            <CartContainer setCart={current_cart}/>
+            <button type="button" className="card-button-shop-all">Checkout</button>
+            </div>)
+            
+            } else {
 
     return (
         <div className="about-us">
             <h1>Shopping Cart</h1>
-            <p>The world is quiet here...</p>
-
-            <button type="button" className="card-button-shop-all">Checkout</button>
-        </div>
-    );
-}
+            <p>The world is quiet here...</p>  
+        </div>)
+    };
+};
 
 
 
