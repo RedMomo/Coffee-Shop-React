044a63f3934d4b73e00acf7accb1042412187aa0
Ameen.Maria.S@gmail.com <redmomo@Marias-MacBook-Air.local>
2022-12-07 14:18:35 -0500

    Deleted half of cart components and started over with rendering the cart

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,7 +1,6 @@
 import React from 'react';
-// import CartContainer from './CartContainer';
 
-function Cart({cart, carts, setCarts}) {
+function Cart() {
 
 
     return (
@@ -9,13 +8,9 @@ function Cart({cart, carts, setCarts}) {
             <h1>Shopping Cart</h1>
             <p>The world is quiet here...</p>
 
-            {/* < CartContainer /> */}
-            {/* < CartContainer carts={carts} setCarts={setCarts}/> */}
-
             <button type="button" className="card-button-shop-all">Checkout</button>
         </div>
     );
-    // include className='login-header' in the div below
 }
 
 
